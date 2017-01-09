abccc68780e7189a66e3b1bae22128c5a61b5384
Dave Verdon <verdond2@gmail.com>
2017-01-09 21:48:14 +0000

    Removed unused icons etc, tidied up sass, js and updated the copy. Also added a favicon.

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,6 +1,5 @@
 /* jshint ignore:start */
 //=include ../bower_components/jquery/dist/jquery.min.js
-//=include ../bower_components/outdated-browser/outdatedbrowser/outdatedbrowser.min.js
 //=include ../bower_components/svg4everybody/dist/svg4everybody.min.js
 /* jshint ignore:end */
 
@@ -20,12 +19,12 @@ jQuery(document).ready(function($){
 	1. Outdated Browser
 	========================================================================== */
 
-	outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform',
-        languagePath: ''
-    });
+	// outdatedBrowser({
+ //        bgColor: '#f25648',
+ //        color: '#ffffff',
+ //        lowerThan: 'transform',
+ //        languagePath: ''
+ //    });
 
 /*  ==========================================================================
     2. SVG 4 Everybody
